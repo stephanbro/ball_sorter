90748bb7557847fa6962806f8173fdc04c8480aa
Stephan Brown <stephan.brown.07@gmail.com>
2016-12-01 18:24:10 -0800

    gate_controller: Add in a module for controlling gates.

---
+++ src/modules/gate_controller.cpp
@@ -0,0 +1,34 @@
+
+#include "gate_controller.hpp"
+#include <string.h>
+
+GateController::GateController()
+{
+  for (uint8_t i = 0; i < SLOT_MAX; i++) {
+    gate_buffer[i] = SLOT_MAX;
+  }
+}
+
+void GateController::tick(slot_t new_dest)
+{
+  // Check each slot to update it's state
+  bool gates_val[SLOT_MAX] = {};
+  for (uint8_t i = 0; i < SLOT_MAX; i++) {
+    if (gate_buffer[i] == i) {
+      gates_val[i] = true;
+      gate_buffer[i] = SLOT_MAX;
+    }
+  }
+  for (uint8_t i = 0; i < SLOT_MAX; i++) {
+    gate_toggle(i, gates_val[i]);
+  }
+  shift_buffer(new_dest);
+}
+
+void GateController::shift_buffer(slot_t new_dest)
+{
+  // Drop the last number off the buffer and shift
+  memmove(&(gate_buffer[1]), &(gate_buffer[0]), sizeof(uint8_t)*3);
+  // Add the newest number to the beginning of the buffer
+  gate_buffer[0] = new_dest;
+}
---
+++ src/modules/gate_controller.hpp
@@ -0,0 +1,23 @@
+
+#include <stdint.h>
+#include <functional>
+
+class GateController
+{
+  public:
+    GateController();
+    virtual ~GateController() {}
+
+    enum slot_t {
+      SLOT1, SLOT2, SLOT3, SLOT4, SLOT_MAX,
+    };
+
+    void tick(slot_t new_dest);
+
+    std::function<void(uint8_t, bool)> gate_toggle;
+
+  private:
+    void shift_buffer(slot_t new_dest);
+
+    uint8_t gate_buffer[SLOT_MAX];
+};
