a9340ece70f2649f4ecc7f3a8fa89406a6436993
Stephan Brown <stephan.brown.07@gmail.com>
2016-12-07 13:23:11 -0800

    gate_controller: Tweak how buffer is shifted. Add some TODOs.

--- src/modules/gate_controller.cpp
+++ src/modules/gate_controller.cpp
@@ -16,6 +16,8 @@ void GateController::tick(gates_t new_dest)
   // Check if we sensed something new
   if (new_dest != GATE_MAX) {
     gates_val[GATE_SENSING] = OPEN;
+    // TODO: An interrupt needs to trigger to close
+    // this gate after a certain amount of time
   }
   // Check each slot to update it's state
   for (uint8_t i = GATE1; i < GATE_MAX; i++) {
@@ -29,13 +31,14 @@ void GateController::tick(gates_t new_dest)
   for (uint8_t i = 0; i < GATE_MAX; i++) {
     gate_toggle(i, gates_val[i]);
   }
+  //TODO: This buffer should not be shifted every tick most likely
   shift_buffer(new_dest);
 }
 
 void GateController::shift_buffer(gates_t new_dest)
 {
-  // Drop the last number off the buffer and shift
-  memmove(&(gate_buffer[1]), &(gate_buffer[0]), sizeof(uint8_t)*3);
   // Add the newest number to the beginning of the buffer
-  gate_buffer[0] = new_dest;
+  gate_buffer[GATE_SENSING] = new_dest;
+  // Shift the buffer along
+  memmove(&(gate_buffer[GATE1]), &(gate_buffer[GATE_SENSING]), sizeof(uint8_t)*(GATE_MAX-1));
 }
