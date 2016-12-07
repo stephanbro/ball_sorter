ef03ac1d3ac56d8638745d22604ac3006fa34a72
Stephan Brown <stephan.brown.07@gmail.com>
2016-12-07 11:39:31 -0800

    gate_controller: Rename slots to gates. Add special handling for the sensing gate.

--- src/modules/gate_controller.cpp
+++ src/modules/gate_controller.cpp
@@ -5,28 +5,34 @@
 GateController::GateController(void (*gate_toggle_in)(uint8_t, uint8_t)) :
   gate_toggle(gate_toggle_in)
 {
-  for (uint8_t i = 0; i < SLOT_MAX; i++) {
-    gate_buffer[i] = SLOT_MAX;
+  for (uint8_t i = 0; i < GATE_MAX; i++) {
+    gate_buffer[i] = GATE_MAX;
   }
 }
 
-void GateController::tick(slot_t new_dest)
+void GateController::tick(gates_t new_dest)
 {
+  bool gates_val[GATE_MAX] = {};
+  // Check if we sensed something new
+  if (new_dest != GATE_MAX) {
+    gates_val[GATE_SENSING] = OPEN;
+  }
   // Check each slot to update it's state
-  bool gates_val[SLOT_MAX] = {};
-  for (uint8_t i = 0; i < SLOT_MAX; i++) {
+  for (uint8_t i = GATE1; i < GATE_MAX; i++) {
     if (gate_buffer[i] == i) {
-      gates_val[i] = true;
-      gate_buffer[i] = SLOT_MAX;
+      gates_val[i] = CLOSE;
+      gate_buffer[i] = GATE_MAX;
+    } else {
+      gates_val[i] = OPEN;
     }
   }
-  for (uint8_t i = 0; i < SLOT_MAX; i++) {
+  for (uint8_t i = 0; i < GATE_MAX; i++) {
     gate_toggle(i, gates_val[i]);
   }
   shift_buffer(new_dest);
 }
 
-void GateController::shift_buffer(slot_t new_dest)
+void GateController::shift_buffer(gates_t new_dest)
 {
   // Drop the last number off the buffer and shift
   memmove(&(gate_buffer[1]), &(gate_buffer[0]), sizeof(uint8_t)*3);
--- src/modules/gate_controller.hpp
+++ src/modules/gate_controller.hpp
@@ -10,18 +10,22 @@ class GateController
     GateController(void (*gate_toggle_in)(uint8_t, uint8_t));
     virtual ~GateController() {}
 
-    enum slot_t {
-      SLOT1, SLOT2, SLOT3, SLOT4, SLOT_MAX,
+    enum gates_t {
+      GATE_SENSING, GATE1, GATE2, GATE3, GATE4, GATE_MAX,
     };
 
-    void tick(slot_t new_dest);
+    enum gate_state_t {
+      OPEN = 0, CLOSE = 1
+    };
+
+    void tick(gates_t new_dest);
 
   private:
     void (*gate_toggle)(uint8_t, uint8_t);
 
-    void shift_buffer(slot_t new_dest);
+    void shift_buffer(gates_t new_dest);
 
-    uint8_t gate_buffer[SLOT_MAX];
+    uint8_t gate_buffer[GATE_MAX];
 };
 
 #endif /* _GATE_CONTROLLER_HPP_ */
