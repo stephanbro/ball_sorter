b3c94110a6206e597c9e22e83fced59f66df1264
Stephan Brown <stephan.brown.07@gmail.com>
2016-12-22 14:57:57 -0800

    tests: Add front check to fifo tests.

--- tests/test_fifo.cpp
+++ tests/test_fifo.cpp
@@ -10,10 +10,12 @@ TEST_CASE( "Buffer pushes and pops normally", "[RingFIFO]" )
   for (uint8_t i = 0; i < 10; i++) {
     REQUIRE( buff.is_full()  == false );
     buff.push(i);
+    REQUIRE( buff.get_size() == i+1 );
     REQUIRE( buff.is_empty() == false );
   }
   REQUIRE( buff.is_empty() == false );
   REQUIRE( buff.is_full()  == true );
+  REQUIRE( buff.front()    == 0 );
 
   for (uint8_t i = 0; i < 10; i++) {
     REQUIRE( buff.is_empty() == false );
@@ -23,5 +25,6 @@ TEST_CASE( "Buffer pushes and pops normally", "[RingFIFO]" )
 
   REQUIRE( buff.is_empty() == true );
   REQUIRE( buff.is_full()  == false );
+  REQUIRE( buff.front()    == RingFIFO::EMPTY );
 }
 
